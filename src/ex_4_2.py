49cdefcc401e00bda16c42e85e741fd062acd9ed
Krishjampana <123447546+Krishjampana@users.noreply.github.com>
2023-02-25 23:10:55 -0600

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -6,7 +6,11 @@ def logstamp_to_datetime(datestr):
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    pass
+    str1 = '%Y-%m-%dT%H:%M:%S'
+    
+    str1_out = datetime.strptime(datestr, str1)
+    
+    return str1_out
 
 
 # >>>> The code below will call your function and print the results
